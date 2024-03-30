5b1852aa24e7ae538cae65182a0be182f993bf4f
susidharthakasss <149891325+susidharthakasss@users.noreply.github.com>
2024-03-30 22:59:54 +0530

    Update BaseClass.java
    change ip

--- src/test/java/testcases/BaseClass.java
+++ src/test/java/testcases/BaseClass.java
@@ -48,7 +48,7 @@ public class BaseClass {
 			options.addArguments("--disable-dev-shm-using") ;
 			options.addArguments("--window-size=1920,1080");
 			options.addArguments("--headless") ;;
-			driver = new RemoteWebDriver(new URL("http://3.87.30.31:4444"),options);
+			driver = new RemoteWebDriver(new URL("http://3.88.28.3:4444"),options);
 
 		}
 		
